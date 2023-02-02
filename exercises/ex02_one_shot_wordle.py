4dfe36f8aa4d6cb74f21c869bb92be880c46d1b4
Yunus Mouline <ymouline@ad.unc.edu>
2023-02-02 16:44:20 -0500

    ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,12 +1,12 @@
-'''One shot Wordle'''
+"""One shot Wordle."""
 __author__ = '730611189'
 
 secret: str = 'python'
 
 guess: str = input('What is your 6-letter guess?')
 while len(guess) != 6:
-  print('That was not 6 letters! Try again:')
-  guess: str = input()
+    print('That was not 6 letters! Try again:')
+    guess: str = input()
 
 
 WHITE_BOX: str = "\U00002B1C"
@@ -14,14 +14,19 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 count: int = 0
-boxes: str  = ''
+boxes: str = ''
 
-while count <=5:
-  if guess[count] == secret[count]: boxes += GREEN_BOX
-  elif guess[count] in secret: boxes += YELLOW_BOX
-  elif guess[count] not in secret: boxes += WHITE_BOX
-  count += 1
+while count <= 5:
+    if guess[count] == secret[count]: 
+        boxes += GREEN_BOX
+    elif guess[count] in secret: 
+        boxes += YELLOW_BOX
+    elif guess[count] not in secret: 
+        boxes += WHITE_BOX
+    count += 1
 
 print(boxes)
-if boxes == GREEN_BOX * 6: print('Woo! You got it!')
-else: print('Not quite. Play again soon.')
+if boxes == GREEN_BOX * 6: 
+    print('Woo! You got it!')
+else: 
+    print('Not quite. Play again soon.')
