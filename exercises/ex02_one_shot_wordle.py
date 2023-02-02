fb7d57c06a87c712838b22c646a78e1bd69a4a04
Yunus Mouline <ymouline@ad.unc.edu>
2023-02-02 17:24:00 -0500

    ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,22 +1,24 @@
 """One shot Wordle."""
+
 __author__ = '730611189'
 
 secret: str = 'python'
 
+#Asking for guess, checking for length
 guess: str = input('What is your 6-letter guess?')
-while len(guess) != 6:
+while len(guess) != len(secret):
     print('That was not 6 letters! Try again:')
     guess: str = input()
 
-
+#stuff
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-
 count: int = 0
 boxes: str = ''
 
-while count <= 5:
+#checking if indexes match, adding green, yellow, or white boxes accordingly
+while count <= len(secret) - 1:
     if guess[count] == secret[count]: 
         boxes += GREEN_BOX
     elif guess[count] in secret: 
@@ -25,8 +27,9 @@ while count <= 5:
         boxes += WHITE_BOX
     count += 1
 
+#outputting guess results
 print(boxes)
-if boxes == GREEN_BOX * 6: 
+if boxes == GREEN_BOX * len(secret): 
     print('Woo! You got it!')
 else: 
     print('Not quite. Play again soon.')
